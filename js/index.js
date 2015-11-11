242c8010b8ef46a31c4901ae835cbd34c6f25a87
sabstag <sabenagupta@gmail.com>
2015-11-10 20:59:36 -0500

    Adding existing code to repo

---
+++ js/index.js
@@ -0,0 +1,34 @@
+$(function(){
+  var main = function() {
+
+  $('.arrownext').click(function() {
+    var currentSlide = $('.active');
+    var nextSlide = currentSlide.next();
+
+    if (nextSlide.length == 0) {
+      nextSlide = $('.review').first();
+    }
+
+    currentSlide.fadeOut(500, function() {
+      $(this).removeClass('active');
+    })
+    nextSlide.fadeIn(500).addClass('active');
+
+  });
+
+  $('.arrowprev').click(function() {
+    var currentSlide = $('.active');
+    var prevSlide = currentSlide.prev();
+
+    if (prevSlide.length == 0) {
+      prevSlide = $('.review').last();
+    }
+
+    currentSlide.fadeOut(500).removeClass('active');
+    prevSlide.fadeIn(500).addClass('active');
+  });
+};
+
+$(document).ready(main);
+
+});
